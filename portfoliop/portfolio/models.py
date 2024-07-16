179da87d09a4a8bf21f2b7272fc54429ed908cb0
Ivan <petrovic.valpovo@gmail.com>
2024-07-16 12:27:40 +0200

    update Experience model

--- portfoliop/portfolio/models.py
+++ portfoliop/portfolio/models.py
@@ -9,10 +9,13 @@ class Experience(models.Model):
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
     year_start = models.IntegerField()
     year_end = models.IntegerField(null=True, blank=True)
     skills_used = models.CharField(max_length=200, blank=True)
-    
+    company_link = models.URLField(blank=True)
+
 
     def __str__(self):
         return self.title
